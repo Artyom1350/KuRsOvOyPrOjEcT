4ae0628d414cddf2ab6f3283de3f06b0876b82e6
lavash2008 <p-bayandin03@mail.ru>
2022-12-12 17:15:31 +0500

    modified:   public/css/app.css 	modified:   public/css/title.css 	new file:   public/image/icons/home.svg 	new file:   public/js/title.js 	modified:   resources/views/home.blade.php 	modified:   resources/views/layouts/main.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title',"Главная")
 
 @section('content')
-<div class="container">
+<div class="container paddingBlock">
     <table>
         <tr>
             <td>день</td>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/title.js') }}" defer></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -34,53 +35,34 @@
         </div>
     </header>
     <main>
-    <div class="d-flex flex-column flex-shrink-0 bg-light sideboard">
-        <a href="/" class="d-block p-3 link-dark text-decoration-none" title="Icon-only" data-bs-toggle="tooltip" data-bs-placement="right">
-            <svg class="bi pe-none" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="visually-hidden">Icon-only</span>
-        </a>
-        <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-            <li class="nav-item">
-            <a href="#" class="nav-link active py-3 border-bottom rounded-0" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Home"><use xlink:href="#home"/></svg>
+        <div class="d-flex flex-column flex-shrink-0 bg-color sideboard">
+            <a href="{{ route('home') }}" class="homeLink p-3 link-dark text-decoration-none" data-bs-toggle="tooltip" data-bs-placement="right">
+                <img src="image/icons/home.svg" class="" alt="Главная">
             </a>
-            </li>
-            <li>
-            <a href="#" class="nav-link py-3 border-bottom rounded-0" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Dashboard"><use xlink:href="#speedometer2"/></svg>
-            </a>
-            </li>
-            <li>
-            <a href="#" class="nav-link py-3 border-bottom rounded-0" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Orders"><use xlink:href="#table"/></svg>
-            </a>
-            </li>
-            <li>
-            <a href="#" class="nav-link py-3 border-bottom rounded-0" title="Products" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Products"><use xlink:href="#grid"/></svg>
-            </a>
-            </li>
-            <li>
-            <a href="#" class="nav-link py-3 border-bottom rounded-0" title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
-                <svg class="bi pe-none" width="24" height="24" role="img" aria-label="Customers"><use xlink:href="#people-circle"/></svg>
-            </a>
-            </li>
-        </ul>
-        <div class="dropdown border-top">
-            <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" class="rounded-circle">
-            </a>
-            <ul class="dropdown-menu text-small shadow">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Sign out</a></li>
+            <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+                <li class="nav-item ">
+                    {{-- active --}}
+                    <a href="#" class="nav-link calendar py-3 border-bottom rounded-0" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right">
+                        Календарь
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link myApplic py-3 border-bottom rounded-0" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right">
+                        Мои заявки
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link applicInProc py-3 border-bottom rounded-0" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right">
+                        Заявки в процессе
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link personArea py-3 border-bottom rounded-0" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right">
+                        Личный кабинет
+                    </a>
+                </li>
             </ul>
-        </div>
-        </div>
-    
-    
+        </div>    
         @yield('content');
     </main>
     <footer>

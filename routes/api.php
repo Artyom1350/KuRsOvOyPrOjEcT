27f50d1f0b9e74ffa0f0eb8bf15037b6cd0917e8
Artem Petrov <Artyom1350@gmail.com>
2023-01-15 17:53:57 +0500

    modified:   resources/views/layouts/main.blade.php 	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -19,14 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
 Route::post('/addApplication',[HomeController::class,'addApplication']);
 Route::get('/getUsers',[HomeController::class,'getUsers']);
 Route::post('/addApplication',[HomeController::class,'addApplication']);
 Route::post('/getDocument',[HomeController::class,'getDocument']);
 Route::post('/changeApplSend',[HomeController::class,'changeApplSend']);
-
-=======
-
-Route::get('/groupsPeople', [DBApiController::class, 'getGroupsPeop']);
->>>>>>> e6a9a40bc05dda522c080efdb34ba4a385760546
